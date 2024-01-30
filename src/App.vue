183699782c500306f458c0e6d0d42da5dee6645b
Xploitable <dannychaindog@gmail.com>
2024-01-30 14:16:13 +0400

    доработан вывод навигационного меня через component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
-const navItems = ['Timeline', 'Activities', 'Progress']
+const navItems = {
+  Timeline: ClockIcon,
+  Activities: ListBulletIcon,
+  Progress: ChartBarIcon,
+}
+
 </script>
 
 <template>
@@ -27,12 +32,9 @@ const navItems = ['Timeline', 'Activities', 'Progress']
 
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="page in navItems" :key="page" class="flex-1">
+      <li v-for="icon, page in navItems" :key="page" class="flex-1">
         <a class="flex flex-col items-center p-2 text-xs capitalize" :href="`#${page.toLowerCase()}`">
-          <ClockIcon v-if="page.toLowerCase() === 'timeline'" class="h-6 w-6"/> 
-          <ListBulletIcon v-else-if="page.toLowerCase() === 'activities'" class="h-6 w-6"/>
-          <ChartBarIcon v-else class="h-6 w-6"/>          
-          {{ page }}
+          <component :is="icon" class="h-6 w-6" /> {{ page }}
         </a>
       </li>
     </ul>
