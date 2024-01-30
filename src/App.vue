5e7ff55a1a2c65509ff9a0a99eeebeb68ef3464b
Xploitable <dannychaindog@gmail.com>
2024-01-30 12:49:05 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = ['Timeline', 'Activities', 'Progress']
+</script>
+
+<template>
+
+  <header class="sticky top-0 z-20 flex justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./assets/logo.png" alt="Logo" class="h-9">
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500"/>
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
+
+  <main class="flex flex-grow flex-col"></main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="page in navItems" :key="page" class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" :href="`#${page.toLowerCase()}`">
+          <ClockIcon v-if="page.toLowerCase() === 'timeline'" class="h-6 w-6"/> 
+          <ListBulletIcon v-else-if="page.toLowerCase() === 'activities'" class="h-6 w-6"/>
+          <ChartBarIcon v-else class="h-6 w-6"/>          
+          {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
