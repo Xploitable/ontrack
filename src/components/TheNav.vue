764a0620bc4769a98be37eaafd3ee78e72e6dccc
Xploitable <dannychaindog@gmail.com>
2024-01-30 14:38:31 +0400

    декомпозиция файла App.vue

---
+++ src/components/TheNav.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+const navItems = {
+  Timeline: ClockIcon,
+  Activities: ListBulletIcon,
+  Progress: ChartBarIcon,
+}
+</script>
+
+<template>
+    <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="icon, page in navItems" :key="page" class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" :href="`#${page.toLowerCase()}`">
+          <component :is="icon" class="h-6 w-6" /> {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
