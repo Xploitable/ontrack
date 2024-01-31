d1a8066f0e3136247c18cbd422d19cbe0dafac00
Xploitable <dannychaindog@gmail.com>
2024-01-31 14:47:50 +0400

    добавлена реактивнсть для подсветки выбранного пункта меню

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import NavItem from './NavItem.vue';
 
@@ -7,13 +8,21 @@ const navItems = {
   Activities: ListBulletIcon,
   Progress: ChartBarIcon,
 }
+
+const currPage = ref('Activities')
 </script>
 
 <template>
     <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="icon, page in navItems" :key="page" :href="`#${page.toLowerCase()}`">
-        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      <NavItem 
+        v-for="icon, page in navItems" 
+        :key="page" 
+        :href="`#${page.toLowerCase()}`" 
+        :class="{'bg-gray-200': page === currPage}"
+        @click="currPage = page"
+        >
+            <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
     </ul>
   </nav>
