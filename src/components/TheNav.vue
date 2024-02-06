157dc2f1226680395a94fb564182c87b05f91ea5
Xploitable <dannychaindog@gmail.com>
2024-02-06 20:09:05 +0400

    little fixes

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -22,7 +22,7 @@
         v-for="(icon, page) in navItems" 
         :key="page" 
         :href="`#${page}`" 
-        :class="{ 'pointer-events-none bg-gray-500': page === currPage }"
+        :class="{ 'pointer-events-none bg-gray-200': page === currPage }"
         @click="emit('navigate', page)"
         >
             <component :is="icon" class="h-6 w-6" /> {{ page }}
